4d0f099a523734206de3c239fa90e12dbb9efec6
dscottfrey <scottfrey@mac.com>
2026-05-03 14:38:54 -0700

    Initial Commit

---
+++ GPXEditor/GPXEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPXEditorApp.swift
+//  GPXEditor
+//
+//  Created by Scott Frey on 5/3/26.
+//
+
+import SwiftUI
+
+@main
+struct GPXEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GPXEditorTests/GPXEditorTests.swift
@@ -0,0 +1,19 @@
+//
+//  GPXEditorTests.swift
+//  GPXEditorTests
+//
+//  Created by Scott Frey on 5/3/26.
+//
+
+import Testing
+@testable import GPXEditor
+
+struct GPXEditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
